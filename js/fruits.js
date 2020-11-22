c1d14ab402678c2c3f60b9595c47301558f6cc9c
Dew2118 <50415635+Dew2118@users.noreply.github.com>
2020-11-22 20:37:22 +0700

    Add files via upload

---
+++ js/fruits.js
@@ -0,0 +1,55 @@
+var seconds = document.getElementById("countdown").textContent;
+var countdown = setInterval(function() {
+    seconds--;
+    document.getElementById("countdown").textContent = seconds;
+    if (seconds <= 0) clearInterval(countdown); 
+}, 1000);
+
+
+ var randomStrings = [
+    "Grape",
+    "Pineapple",
+    "Avocado",
+    "Blueberries",
+    "Apples",
+    "Pomegranate",
+    "Mango",
+    "Strawberries",
+    "Cranberries",
+    "Lemons",
+    "Durian",
+    "Watermelon",
+    "Olives",
+    "Blackberries",
+    "Oranges",
+    "Bananas",
+    "Guava",
+    "Papaya",
+    "Cherries",
+    "Linchi",
+    "Peach"
+  ];
+
+  var randomSpan = document.getElementById("word");
+
+  document.getElementById("correct").addEventListener("click", generate);
+   document.getElementById("pass").addEventListener("click", generate);
+  function generate() {
+    randomIndex = Math.ceil((Math.random() * randomStrings.length - 1));
+    newText = randomStrings[randomIndex];
+    randomSpan.innerHTML = newText;
+  }
+
+  generate();
+ 
+ var points = document.getElementById("points"); var total = 0;
+function addPoint() {
+    total++;
+    points.innerHTML = total;
+}
+
+function minusPoint() {
+    total--;
+    points.innerHTML = total;
+}
+
---
+++ js/sports.js
@@ -0,0 +1,55 @@
+var seconds = document.getElementById("countdown").textContent;
+var countdown = setInterval(function() {
+    seconds--;
+    document.getElementById("countdown").textContent = seconds;
+    if (seconds <= 0) clearInterval(countdown); 
+}, 1000);
+
+
+ var randomStrings = [
+    "Soccer/Football",
+    "Badminton",
+    "Hockey",
+    "Volleyball",
+    "Basketball",
+    "Tennis",
+    "Cricket",
+    "Table Tennis",
+    "Baseball",
+    "Golf",
+    "Gymnastic",
+    "Long Jump",
+    "High Jump",
+    "American Football",
+    "Rugby",
+    "Boxing",
+    "Bowling",
+    "Swimming",
+    "Cycling",
+    "Paintball",
+    "Parkour",
+  ];
+
+  var randomSpan = document.getElementById("word");
+
+  document.getElementById("correct").addEventListener("click", generate);
+   document.getElementById("pass").addEventListener("click", generate);
+  function generate() {
+    randomIndex = Math.ceil((Math.random() * randomStrings.length - 1));
+    newText = randomStrings[randomIndex];
+    randomSpan.innerHTML = newText;
+  }
+
+  generate();
+ 
+ var points = document.getElementById("points"); var total = 0;
+function addPoint() {
+    total++;
+    points.innerHTML = total;
+}
+
+function minusPoint() {
+    total--;
+    points.innerHTML = total;
+}
+
